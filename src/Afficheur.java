23bd0f792e52668bdf6f29d2e3be8b160ae133c8
Alcide <paul.robin@etu.unistra.fr>
2018-05-17 21:53:03 +0200

    wtf

--- src/Afficheur.java
+++ src/Afficheur.java
@@ -1,5 +1,7 @@
 import javax.swing.JFrame;
+import java.awt.Graphics;
 import javax.swing.JPanel;
+
 
 public class Afficheur extends JFrame {
 	public static void main(String[] args) {
@@ -9,13 +11,18 @@ public class Afficheur extends JFrame {
 	public Afficheur() {
 			int height = 400;
 			int width = 400;
-			this.setSize(height, width);
 			this.setLocationRelativeTo(null);
 			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 			
-			Panneau pan = Panneau();
-			
+			JPanel pan = new Panneau(height,width);
+			this.setContentPane(pan);
+			this.pack();
+			Point p1 = new Point(10,10);
+			Point p2 = new Point(310,310);
+			Graphics g = pan.getGraphics();
+			pan.paintComponent(g);
 			this.setVisible(true);
 	}
-}
+	
+
 
--- src/Panneau.java
+++ src/Panneau.java
@@ -1,9 +1,17 @@
 import java.awt.Graphics;
 import javax.swing.JPanel;
+import java.awt.Dimension;
+import java.util.ArrayList;
 
 public class Panneau extends JPanel{
-	public Panneau() {}
-	public void traceLigne(Graphics g, Point p1, Point p2) {
+	
+	ArrayList<Point> list;
+	public Panneau(int h, int w, ArrayList<Point> list){
+		setPreferredSize(new Dimension(h,w));
+		this.list = list;
+	}
+	
+	public void paintComponent(Graphics g) {
 		g.drawLine(p1.x,p1.y,p2.x,p2.y);
 	}
 }
